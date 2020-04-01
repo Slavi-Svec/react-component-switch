d76675e8156a3af9fa8489eb8b0786d1e856b78e
izaak svec <slavi.svec@gmail.com>
2020-04-01 17:50:55 +1000

    add css style sheet, change background

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Switch from 'react-switch';
+// import Style from'./Components/Style.css';
+import './Style.css'
 
 
 class App extends React.Component {
     constructor () {
         super ()
         this.state = {
+            checked: false
 
         }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    handleChange(checked) {
+        this.setState({checked})
     }
     render () {
         return (
-            <div>
-            hello 
+            <div class="wrpper">
+                <h2>IOS Switch</h2>
+                    <Switch
+                        className="react-switch"
+                        onChange={this.handleChange}
+                        checked={this.state.checked}  
+                    />
+                    <p>the switch is <b>{this.state.checked ? 'on' : 'off' }</b></p>
             </div>
         )
     }
