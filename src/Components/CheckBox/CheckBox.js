841f065982769909d201a2f54197db8425dc3f0a
izaak svec <slavi.svec@gmail.com>
2020-04-03 09:39:58 +1000

    Add style to background

--- src/Components/CheckBox/CheckBox.js
+++ src/Components/CheckBox/CheckBox.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react'
 import Switch from 'react-switch'
 import RenderState from '../RenderState/RenderState'
+import './Style.css'
 
 const CheckBox = () => {
   const [checked, handelChange] = useState(false)
     return (
         <>
+          <div>
+          <h1 style={{backgroundColor: 'pink'}}>hello</h1>
           <Switch onChange={() => handelChange(!checked)} checked={checked} />
           <RenderState text="The switch is" checked={checked} />
+          </div>
         </>
 
     )
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const App = () => {
     return (
         <>
             <CheckBox />
-            <CheckBoxV2 />
+            {/* <CheckBoxV2 />  */}
         </>
     )
 }
